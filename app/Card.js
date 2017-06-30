f5a4290545c9f360357d15debee04ee06bbfb166
KK <whshshshs@foxmail.com>
2017-06-30 17:46:55 +0800

    使用State切换卡片

--- app/Card.js
+++ app/Card.js
@@ -1,20 +1,53 @@
-import React , {Component} from 'react';
+import React, {Component} from 'react';
 import CheckList from './CheckList';
 
-class Card extends Component{
-  render(){
-    return (
-      <div className = "card">
-        <div className = "card_title">{this.props.title}</div>
+class Card extends Component {
+	constructor() {
+		super(...arguments);
+		this.state = {
+			showDetails: false
+		};
+	}
 
-        <div className="card_details">
-          {this.props.description}
-          <CheckList cardId = {this.props.id} tasks = {this.props.tasks} />
-        </div>
+	toggleDetails() {
+		this.setState({showDetails: !this.state.showDetails});
+	}
 
-      </div>
-    );
-  }
+	render() {
+		let cardDetails;
+
+		if (this.state.showDetails) {
+			cardDetails = (
+				<div className="card_details">
+					{this.props.description}
+					<CheckList cardId={this.props.id}
+					           tasks={this.props.tasks} />
+
+
+				</div>
+
+
+			)
+		}
+
+		return (
+			<div className="card"
+
+			>
+				<div className={this.state.showDetails ? "card_title card_title--is--open" : "card_title"}
+				     onClick={
+					     this.toggleDetails.bind(this)
+				     }>
+					{this.props.title}
+				</div>
+
+				{cardDetails}
+
+
+
+			</div>
+		);
+	}
 }
 
 export default Card;
