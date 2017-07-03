e8f3da6eff8b962c4daf906d648bd162c0bd14c7
KK <whshshshs@foxmail.com>
2017-07-03 15:17:19 +0800

    Sort out app folder.

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var webpack = require('webpack');
  */
 var config = {
   devtool: 'eval-source-map',
-  entry:  __dirname + "/app/TodoApp.js",
+  entry:  __dirname + "/app/TodoApp/TodoApp.js",
   output: {
     path: __dirname + "/public",
     filename: "bundle.js"
