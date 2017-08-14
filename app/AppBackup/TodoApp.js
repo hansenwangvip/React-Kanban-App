27f512a9b69df23a5812556b20317a009130c3ca
jasonwithjs <1197913035@qq.com>
2017-08-14 20:58:21 +0800

    精简项目目录

---
+++ app/AppBackup/TodoApp.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {render} from 'react-dom';
+import KanbanBoardContainer from './KanbanBoardContainer';
+
+
+
+render(<KanbanBoardContainer />, document.getElementById("root"));
